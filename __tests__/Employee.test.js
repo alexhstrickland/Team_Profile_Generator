49f6138f7821a9b5256ca285fec9a13228df6e45
alexhstrickland <alexhstrickland@gmail.com>
2021-02-11 12:40:19 -0500

    added test for setting an employee name via constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,4 +3,11 @@ const Employee = require("..lib/Employee");
 test("Can instantiate Employee instance", () => {
     const emp = new Employee();
     expect(typeof(emp)).toBe("object");
-})
+});
+
+test("Can set employee name using Employee constructor", () => {
+    const name = "Alex";
+    const emp = new Employee(name);
+
+    expect(emp.name).toBe(name);
+});
