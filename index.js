5664df48de4028ccda5a3e9d341c64dd74ec9faf
alexhstrickland <alexhstrickland@gmail.com>
2021-02-11 12:26:19 -0500

    added tests folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-
+// node index.js should trigger inquirer questions: "Please bui;d your team" team managers name, tm id, tm email, tm office number, which team member would you like to add? (choices), engineer or intern or i don't want to add anymore, 
+// engineer: name, id, email, github username
+// intern: name, id, email, school
 
 const HTML =
 `
