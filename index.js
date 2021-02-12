3ec156b0b1a106ebf2ef7b38a2568f3be59cbbde
alexhstrickland <alexhstrickland@gmail.com>
2021-02-12 17:16:17 -0500

    added inquirer prompt content

--- index.js
+++ index.js
@@ -2,6 +2,132 @@
 // engineer: name, id, email, github username
 // intern: name, id, email, school
 
+	inquirer.prompt([
+		{
+			type: "input",
+			message: "Please enter the team manager's name: ",
+			name: "manName",	
+		},
+		{
+			type: "input",
+			message: "Please enter the team manager's ID: ",
+			name: "manID",
+		},
+		{
+			type: "input",
+			message: "Please enter the team manager's office number: ",
+			name: "manNumber",
+		},
+		{
+			type: "list",
+			message: "Which team member would you like to add?",
+			name: "role"
+			choices: ["Engineer, Intern, No more team members"]
+		}
+	])
+	.then(data => {
+			
+		if(data.role === "Engineer") {
+			engineerInfo();
+		} else if(data.role === "Intern") {
+			internInfo();
+		} else if(data.role === "No more team members") {
+			noMore();
+		}
+
+	])
+
+	function engineerInfo() {
+		inquirer
+			.prompt([
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				}
+			])
+	}
+
+	function internInfo() {
+		inquirer
+			.prompt([
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				},
+				{
+					type: "",
+					message: "",
+					name: "",
+				}
+
+			])
+	}
+
+	function noMore () {
+		inquirer
+			.prompt([
+				{
+					type: "",
+					message: "",
+					name: "",
+				}
+			])
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const HTML =
 `
 <html>
@@ -18,4 +144,4 @@ const HTML =
 	</div>
 </body>
 </html>
-`;
+`; 
