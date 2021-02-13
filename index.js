00cbb452cf2898b33aadfc9062d25c6aa1b30e02
alexhstrickland <alexhstrickland@gmail.com>
2021-02-13 14:14:29 -0500

    changes

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // node index.js should trigger inquirer questions: "Please bui;d your team" team managers name, tm id, tm email, tm office number, which team member would you like to add? (choices), engineer or intern or i don't want to add anymore, 
 // engineer: name, id, email, github username
 // intern: name, id, email, school
-
-	inquirer.prompt([
-		{
-			type: "input",
-			message: "Please enter the team manager's name: ",
-			name: "managerName",	
-		},
-		{
-			type: "input",
-			message: "Please enter the team manager's ID: ",
-			name: "managerID",
-		},
-		{
-			type: "input",
-			message: "Please enter the team manager's office number: ",
-			name: "managerNumber",
-		},
-		{
-			type: "list",
-			message: "Which team member would you like to add?",
-			name: "role",
-			choices: ["Engineer", "Intern", "No more team members"]
-		}
-	])
+function getInfo() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "Please enter the team manager's name: ",
+				name: "managerName",	
+			},
+			{
+				type: "input",
+				message: "Please enter the team manager's ID: ",
+				name: "managerID",
+			},
+			{
+				type: "input",
+				message: "Please enter the team manager's office number: ",
+				name: "managerNumber",
+			},
+			{
+				type: "list",
+				message: "Which team member would you like to add?",
+				name: "role",
+				choices: ["Engineer", "Intern", "No more team members"]
+			}
+		])
 	.then(data => {
 			
 		if(data.role === "Engineer") {
@@ -35,79 +36,85 @@
 			noMore();
 		}
 
-	});
+	})
+};
 
-	function engineerInfo() {
-		inquirer
-			.prompt([
-				{
-					type: "input",
-					message: "Please enter the name of the engineer: ",
-					name: "engineerName",
-				},
-				{
-					type: "input",
-					message: "Please enter the engineer's ID: ",
-					name: "engineerID",
-				},
-				{
-					type: "input",
-					message: "Please enter the engineer's email: ",
-					name: "engineerEmail",
-				},
-				{
-					type: "input",
-					message: "Please enter the engineer's GitHub username: ",
-					name: "engineerGitHub",
-				},
-				{
-					type: "list",
-					message: "Which team member would you like to add?",
-					name: "role",
-					choices: ["Engineer", "Intern", "No more team members"],
-				}
-			])
-	}
 
-	function internInfo() {
-		inquirer
-			.prompt([
-				{
-					type: "input",
-					message: "Please enter the name of the intern: ",
-					name: "internName",
-				},
-				{
-					type: "input",
-					message: "Please enter the intern's ID: ",
-					name: "internID",
-				},
-				{
-					type: "input",
-					message: "Please enter the intern's email: ",
-					name: "internEmail",
-				},
-				{
-					type: "input",
-					message: "Please enter the intern's school: ",
-					name: "internSchool",
-				},
-				{
-					type: "list",
-					message: "Which team member would you like to add?",
-					name: "role",
-					choices: ["Engineer", "Intern", "No more team members"],
-				}
+function engineerInfo() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "Please enter the name of the engineer: ",
+				name: "engineerName",
+			},
+			{
+				type: "input",
+				message: "Please enter the engineer's ID: ",
+				name: "engineerID",
+			},
+			{
+				type: "input",
+				message: "Please enter the engineer's email: ",
+				name: "engineerEmail",
+			},
+			{
+				type: "input",
+				message: "Please enter the engineer's GitHub username: ",
+				name: "engineerGitHub",
+			},
+			{
+				type: "list",
+				message: "Which team member would you like to add?",
+				name: "role",
+				choices: ["Engineer", "Intern", "No more team members"],
+			}
+		])
+}
 
-			])
-	}
+function internInfo() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "Please enter the name of the intern: ",
+				name: "internName",
+			},
+			{
+				type: "input",
+				message: "Please enter the intern's ID: ",
+				name: "internID",
+			},
+			{
+				type: "input",
+				message: "Please enter the intern's email: ",
+				name: "internEmail",
+			},
+			{
+				type: "input",
+				message: "Please enter the intern's school: ",
+				name: "internSchool",
+			},
+			{
+				type: "list",
+				message: "Which team member would you like to add?",
+				name: "role",
+				choices: ["Engineer", "Intern", "No more team members"],
+			}
 
-	function noMore () {
-	// this function will end prompt and send data to create cards
-	}
+		])
+}
 
-	
+function noMore () {
+// this function will end prompt and send data to create cards
+}
+
+
+// getInfo();
+
 
 
 
 // need to write a for loop to dynamically add boostrap cards. Also will need if statements in order to diaplay appropriate icon for role
+
+
