7ba3f5a02f85a61af8a259b5c412c61a0bfca291
alexhstrickland <alexhstrickland@gmail.com>
2021-02-13 10:17:47 -0500

    added html structure

--- index.js
+++ index.js
@@ -35,7 +35,7 @@
 			noMore();
 		}
 
-	])
+	});
 
 	function engineerInfo() {
 		inquirer
@@ -110,33 +110,3 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const HTML =
-`
-<html>
-<head>
-	<title>Page</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
-	<div class="jumbotron" style="background-color: red; color: white">
-		<h1 class="text-center">My Team</h1>
-	</div>
-	<div class="container">
-		
-	</div>
-</body>
-</html>
-`; 
