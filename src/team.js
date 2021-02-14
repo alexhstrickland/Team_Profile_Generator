6614e53663b399559703280061165511b05bfe89
alexhstrickland <alexhstrickland@gmail.com>
2021-02-14 14:44:26 -0500

    deleted unnecessary html file and added render function to src file

---
+++ src/team.js
@@ -0,0 +1,23 @@
+function generateCards() {
+    
+}
+
+function render() {
+    `
+    <html>
+    <head>
+        <title>Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../src/style.css">
+    </head>
+    <body>
+        <div class="jumbotron">
+            <h1 class="text-center">My Team</h1>
+        </div>
+        <div class="container">
+            
+        </div>
+    </body>
+    </html>
+    `
+}
